eb3c186f7e7b65190c9a02c43a15c1a68737c5bc
viniciushoffmanndev <viniciushoffmanndev@users.noreply.github.com>
2026-05-01 18:34:17 -0300

    feat(validation): create viewset to handle crud operations for financial assets

--- validation/views.py
+++ validation/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import FinancialOperation
+from .serializers import FinancialOperationSerializer
 
-# Create your views here.
+class FinancialOperationViewSet(viewsets.ModelViewSet):
+    queryset = FinancialOperation.objects.all().order_by('-created_at')
+    serializer_class = FinancialOperationSerializer
