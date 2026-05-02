6fbd22d78920fe19790a921d14ce8f0fa47020ab
viniciushoffmanndev <viniciushoffmanndev@users.noreply.github.com>
2026-05-02 00:21:56 -0300

    feat(validation): integrate kafka producer into operation creation workflow

--- validation/views.py
+++ validation/views.py
@@ -1,7 +1,28 @@
+# validation/views.py
 from rest_framework import viewsets
 from .models import FinancialOperation
 from .serializers import FinancialOperationSerializer
+from .producer import send_operation_to_kafka
 
 class FinancialOperationViewSet(viewsets.ModelViewSet):
     queryset = FinancialOperation.objects.all().order_by('-created_at')
     serializer_class = FinancialOperationSerializer
+
+    def perform_create(self, serializer):
+        # Salva o registro no PostgreSQL
+        operation = serializer.save()
+        
+        # Prepara os dados para o Kafka
+        operation_data = {
+            'id': str(operation.id),
+            'asset_code': operation.asset_code,
+            'issuer': operation.issuer,
+            'volume': str(operation.volume),
+            'status': operation.status,
+        }
+        
+        # Envia para o Kafka
+        try:
+            send_operation_to_kafka(operation_data)
+        except Exception as e:
+            print(f"Erro ao enviar para o Kafka: {e}")
